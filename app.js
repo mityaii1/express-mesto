59edd040b6003ec0f5f043965fb0a94bcc979f97
mityaii1 <63926278+mityaii1@users.noreply.github.com>
2020-10-10 22:42:37 +0500

    Проект №12

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const { PORT = 3000 } = process.env;
+const path = require('path');
+const router = require('./routes/index')
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', router)
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`)
+})
+
